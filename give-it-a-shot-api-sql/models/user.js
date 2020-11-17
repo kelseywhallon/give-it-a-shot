d25b8e0c4f334752590306312f54a75714fb98b7
Kelsey Whallon <71531206+kelseywhallon@users.noreply.github.com>
2020-11-17 13:42:59 -0500

    Update user.js

--- give-it-a-shot-api-sql/models/user.js
+++ give-it-a-shot-api-sql/models/user.js
@@ -14,6 +14,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+        models.user.belongsToMany(models.drink, {
+        through: "favorites"
     }
     validPassword(passwordTyped) {
       return bcrypt.compareSync(passwordTyped, this.password);
