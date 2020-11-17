549001def12865498abcbfd279f575fdcbcad9f8
Derek Foster <derekfoster94@gmail.com>
2020-11-17 11:52:41 -0800

    updated code to handle last name, only show header once logged in

--- give-it-a-shot-api-sql/controllers/auth.js
+++ give-it-a-shot-api-sql/controllers/auth.js
@@ -1,46 +1,56 @@
-const db = require('../models')
+const db = require("../models");
 
 const login = (req, res) => {
-    res.json({ user: req.user.id })
-}
+  res.json({ user: req.user.id });
+};
 
 const register = (req, res) => {
-    const { name, email, password } = req.body
+  const { firstName, lastName, email, password } = req.body;
 
-    // validate the POSTed data - making sure we have a name, an email, a pw
-    if (!name || !email || !password) {
-        return res.json({ message: 'Please enter a name, an email, and a password' })
-    }
+  // validate the POSTed data - making sure we have a name, an email, a pw
+  if (!firstName || !lastName || !email || !password) {
+    return res.json({
+      message:
+        "Please enter a first name, a last name, an email, and a password"
+    });
+  }
 
-    // make sure the user doesn't already exist
-    db.user.findOne({ where: { email } })
-        .then(foundUser => {
-            if (foundUser) {
-                return res.json({ message: "A user with that email already exists" })
-            }
+  // make sure the user doesn't already exist
+  db.user
+    .findOne({ where: { email } })
+    .then(foundUser => {
+      if (foundUser) {
+        return res.json({ message: "A user with that email already exists" });
+      }
 
-            // if the user doesnt exist, create and save a user to the DB
-            db.user.create({
-                name,
-                email,
-                password
-            }).then(newUser => {
-                console.log('New user created!')
-                res.json(newUser)
-            })
+      // if the user doesnt exist, create and save a user to the DB
+      db.user
+        .create({
+          firstName,
+          lastName,
+          email,
+          password
         })
-}
+        .then(newUser => {
+          console.log("New user created!");
+          res.json(newUser);
+        });
+    })
+    .catch(error => {
+      console.err(error.toString());
+    });
+};
 
 const logout = (req, res) => {
-    if (!req.user) {
-        return res.json({ message: 'No User to log out' })
-    }
-    req.logout()
-    res.json({ message: "User logged out" })
-}
+  if (!req.user) {
+    return res.json({ message: "No User to log out" });
+  }
+  req.logout();
+  res.json({ message: "User logged out" });
+};
 
 module.exports = {
-    login,
-    register,
-    logout
-}
+  login,
+  register,
+  logout
+};
