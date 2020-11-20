1348d9616ea860256707ff872296b85a72cc5514
Derek Foster <derekfoster94@gmail.com>
2020-11-20 14:37:49 -0800

    first go at css grid, added to index.html to ensure the app takes up entire screen

--- give-it-a-shot-client/src/pages/Home/Home.js
+++ give-it-a-shot-client/src/pages/Home/Home.js
@@ -1,20 +1,27 @@
 import React from "react";
 import icon from "./assets/alcohol_icon.png";
 import ButtonLink from "../../components/ButtonLink";
+import "./style.scss";
 
 export const Home = props => {
-console.log(props)
-  return props.currentUser ? (
-    <div>
-      <h1>Welcome Home.. Grab a Drink 🍸 </h1>
-      <h2>Profile of user with ID {props.currentUser}</h2>
-      <ButtonLink path="/quiz" text="Take the drink quiz" />
+  console.log(props);
+  return (
+    <div className="home">
+      {props.currentUser ? (
+        <>
+          <h1>Welcome Home.. Grab a Drink 🍸 </h1>
+          <h2>Profile of user with ID {props.currentUser}</h2>
+          <ButtonLink path="/quiz" text="Take the drink quiz" />
+        </>
+      ) : (
+        <>
+          <img className="main-icon" src={icon} alt="" />
+          <div className="home-actions">
+            <ButtonLink path="/register" text="Register" />
+            <ButtonLink path="/login" text="Login" />
+          </div>
+        </>
+      )}
     </div>
-  ) : (
-    <>
-      <img src={icon} alt="" />
-      <ButtonLink path="/register" text="Register" />
-      <ButtonLink path="/login" text="Login" />
-    </>
   );
 };
