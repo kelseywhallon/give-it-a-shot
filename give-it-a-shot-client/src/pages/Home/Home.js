9fd6500caf5a0741bc31f9cc17563e5324980078
Derek Foster <derekfoster94@gmail.com>
2020-11-29 19:01:18 -0800

    home page w/icons + responsive

--- give-it-a-shot-client/src/pages/Home/Home.js
+++ give-it-a-shot-client/src/pages/Home/Home.js
@@ -1,31 +1,52 @@
-import React from "react";
-import icon from "./assets/alcohol_icon.png";
+import React, { useState } from "react";
+import loggedOutIcon from "./assets/alcohol_icon.png";
+import shaker from "./assets/cocktailshaker.png";
+import martini from "./assets/martini.png";
+import mimosa from "./assets/mimosa.png";
+import pineapple from "./assets/pineapple.png";
 import { ButtonLink } from "../../components/Button";
 import styles from "./assets/Home.module.scss";
+import { vw, mobileBreakpoint, getViewport } from "../../utility";
 
 export const Home = props => {
-  console.log(props);
+  const [desktop, setDesktop] = useState(vw > mobileBreakpoint ? true : false);
+
+  const checkViewport = () => {
+    const [vw, vh] = getViewport();
+
+    if (vw > mobileBreakpoint) {
+      setDesktop(true);
+    } else if (vw <= mobileBreakpoint) {
+      setDesktop(false);
+    }
+  };
+
+  window.addEventListener("resize", checkViewport);
+
   return (
-    <div className={styles.home}>
+    <>
       {props.currentUser ? (
-        <>
-          <h2 className={`${styles.mainIcon} ${styles.centered}`}>
-            Welcome Home.. Grab a Drink 🍸{" "}
-          </h2>
-          {/* <h2 className={`${styles.home2} ${styles.centered}`}>
-            Profile of user with ID {props.currentUser}
-          </h2> */}
+        <div className={styles.loggedIn}>
+          <h2>Welcome Home.. Grab a Drink 🍸</h2>
+          <div className={styles.grid}>
+            <img src={shaker} alt="" />
+            <img src={mimosa} alt="" />
+            <img src={pineapple} alt="" />
+            <img src={martini} alt="" />
+          </div>
+
           <ButtonLink
-            className={`${styles.home3} ${styles.centered}`}
+            large={desktop}
+            className={styles.button}
             path="/quiz"
             text="Take the drink quiz"
           />
-        </>
+        </div>
       ) : (
-        <>
+        <div className={styles.home}>
           <img
             className={`${styles.mainIcon} ${styles.centered}`}
-            src={icon}
+            src={loggedOutIcon}
             alt=""
           />
           <ButtonLink
@@ -38,8 +59,8 @@ export const Home = props => {
             path="/register"
             text="Register"
           />
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 };
