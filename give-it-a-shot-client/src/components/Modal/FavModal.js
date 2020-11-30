820643eb5dab5bff62afae341398a41a38123f34
Derek Foster <derekfoster94@gmail.com>
2020-11-30 11:25:16 -0800

    resolved minor warnings

--- give-it-a-shot-client/src/components/Modal/FavModal.js
+++ give-it-a-shot-client/src/components/Modal/FavModal.js
@@ -1,36 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import Modal from 'react-bootstrap/Modal'
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 import { Button } from "../Button";
 
 export const FavModal = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    return (
-        <>
-            <Button onClick={() => setShow(true)}>
-                Custom Width Modal
-            </Button>
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Custom Width Modal</Button>
 
-            <Modal
-                show={show}
-                onHide={() => setShow(false)}
-                dialogClassName="modal-50w"
-                aria-labelledby="example-custom-modal-styling-title"
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="example-custom-modal-styling-title">
-                        Success! 
-            </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>
-                        This drink has been added to your favorites! 
-                    </p>
-                </Modal.Body>
-            </Modal>
-        </>
-    );
-}
-
-
-
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-50w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Success!
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>This drink has been added to your favorites!</p>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
--- give-it-a-shot-client/src/pages/DrinkDetails/DrinkDetails.js
+++ give-it-a-shot-client/src/pages/DrinkDetails/DrinkDetails.js
@@ -6,72 +6,67 @@ import ListGroupItem from "react-bootstrap/ListGroupItem";
 import styles from "./Details.module.scss";
 import { Button } from "../../components/Button";
 
-
 export const DrinkDetails = props => {
-    // const backListener = props.history.listen((location, action) => {
-    //     if (action === "POP") {
-    //         console.log("back back to results");
-    //     }
-    // });
+  // const backListener = props.history.listen((location, action) => {
+  //     if (action === "POP") {
+  //         console.log("back back to results");
+  //     }
+  // });
 
-    const [idDrink, setIdDrink] = useState("");
-    const [drink, setDrinkDetails] = useState({});
+  const [drink, setDrinkDetails] = useState({});
 
-    const getDrinks = () => {
-        let location = props.history.location;
-        let idDrink = location.pathname.slice(7);
+  const getDrinks = () => {
+    let location = props.history.location;
+    let idDrink = location.pathname.slice(7);
 
-        DrinksApi.getDrinkDetails(idDrink).then(data => {
-            setIdDrink(idDrink);
-            setDrinkDetails(data[0]);
-        });
-    };
+    DrinksApi.getDrinkDetails(idDrink).then(data => {
+      setDrinkDetails(data[0]);
+    });
+  };
 
-    useEffect(() => {
-        getDrinks();
-    }, []);
+  useEffect(getDrinks, [props.history.location]);
 
-    return (
-        <div className={`${styles.container} ${styles.options}`}>
-            <Card className={`${styles.card}`}>
-                <Card.Img
-                    className={`${styles.cardImg}`}
-                    variant="top"
-                    src={drink.strDrinkThumb}
-                />
-                <Card.Body className={`${styles.cardBody}`}>
-                    <h1 className={`${styles.drinkName}`}> {drink.strDrink} </h1>
-                    <Card.Text className={`${styles.category}`}>
-                        <div>
-                            <h5>Drink Category:</h5>
-                            {drink.strCategory}
-                        </div>
-                        <div>
-                            <h5>Type of Glass:</h5>
-                            {drink.strGlass}
-                        </div>
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className={`${styles.ingredients}`}>
-                    <h5>Ingredients:</h5>
-                    <ListGroupItem>{drink.strIngredient1}</ListGroupItem>
-                    <ListGroupItem>{drink.strIngredient2}</ListGroupItem>
-                    <ListGroupItem>{drink.strIngredient3}</ListGroupItem>
-                    <ListGroupItem>{drink.strIngredient4}</ListGroupItem>
-                </ListGroup>
-                <Card.Footer>
-                    <Button
-                        className={`${styles.detailButton}`}
-                        type="submit"
-                        onClick={() =>
-                            props.history.push({
-                                pathname: "/quiz"
-                            })
-                        }
-                        content="Back to Results"
-                    />
-                </Card.Footer>
-            </Card>
-        </div>
-    );
+  return (
+    <div className={`${styles.container} ${styles.options}`}>
+      <Card className={`${styles.card}`}>
+        <Card.Img
+          className={`${styles.cardImg}`}
+          variant="top"
+          src={drink.strDrinkThumb}
+        />
+        <Card.Body className={`${styles.cardBody}`}>
+          <h1 className={`${styles.drinkName}`}> {drink.strDrink} </h1>
+          <Card.Text className={`${styles.category}`}>
+            <div>
+              <h5>Drink Category:</h5>
+              {drink.strCategory}
+            </div>
+            <div>
+              <h5>Type of Glass:</h5>
+              {drink.strGlass}
+            </div>
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className={`${styles.ingredients}`}>
+          <h5>Ingredients:</h5>
+          <ListGroupItem>{drink.strIngredient1}</ListGroupItem>
+          <ListGroupItem>{drink.strIngredient2}</ListGroupItem>
+          <ListGroupItem>{drink.strIngredient3}</ListGroupItem>
+          <ListGroupItem>{drink.strIngredient4}</ListGroupItem>
+        </ListGroup>
+        <Card.Footer>
+          <Button
+            className={`${styles.detailButton}`}
+            type="submit"
+            onClick={() =>
+              props.history.push({
+                pathname: "/quiz"
+              })
+            }
+            content="Back to Results"
+          />
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 };
